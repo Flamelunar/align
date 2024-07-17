e905d6da991bcddedea2dd235c98b1d56d76768f
Anonymity <132193042+noteljj@users.noreply.github.com>
2024-07-17 20:46:43 +0800

    Update bertembed.py

--- src/bertembed.py
+++ src/bertembed.py
@@ -20,7 +20,7 @@ class Bert_Embedding(nn.Module):
             self.freeze()
 
     def forward(self, subword_idxs, subword_masks, token_starts_masks, sens): #[57,22]
-        sen_lens = token_starts_masks.sum(dim=1)  # 句子中的单词个数
+        sen_lens = token_starts_masks.sum(dim=1) 
         final_outs, hidden_outs = self.bert(
             subword_idxs,#[57,22]
             token_type_ids=None,
@@ -35,17 +35,17 @@ class Bert_Embedding(nn.Module):
         # print("hidden_outs")
         # print(hidden_outs)
         # print("len(hidden_outs)")
-        # print(len(hidden_outs))#13层
+        # print(len(hidden_outs))#
         # print("hidden_outs[-1]")
         # print(hidden_outs[-1])
-        bert_outs = hidden_outs #13层
+        bert_outs = hidden_outs #
         # output_all_encoded_layers=True,
-        bert_outs = bert_outs[len(bert_outs) - self.bert_layer: len(bert_outs)]  # 获取后四层的表示，这个是一个list，里面存放了所有层的表示
+        bert_outs = bert_outs[len(bert_outs) - self.bert_layer: len(bert_outs)]  
         # print("embed bert outs bert_outs[0].size()",bert_outs[0].size())
         # rint("embed bert outs",bert_outs.shape)
-        bert_outs = self.scalar_mix(bert_outs)  # 输入是一个list里面有四个[3,10,768],输出是一个[3,10,768]  vi[57,22,768]
+        bert_outs = self.scalar_mix(bert_outs)  
         # print("embed after scalar mix",bert_outs.size())
-        bert_outs = torch.split(bert_outs[token_starts_masks], sen_lens.tolist())  # 输出是一个tuple,每个是一个句子的tensor[词数，768]
+        bert_outs = torch.split(bert_outs[token_starts_masks], sen_lens.tolist())  
         # print(bert_outs[token_starts_masks])
         for i in range(len(bert_outs)):
             if (bert_outs[i].size()[0] != sum(token_starts_masks[i]) and bert_outs[i].size()[0] != len(sens[i])):
@@ -56,10 +56,10 @@ class Bert_Embedding(nn.Module):
         # print("embed after split bert outs ",bert_outs[0].size())#[4, 768]
         # print("embed after split bert outs ",bert_outs[1].size())#[3, 768]
         # print("embed after split bert outs ",bert_outs.size())
-        bert_outs = pad_sequence(bert_outs, batch_first=True) # vi输出是一个tensor[55,10,768]
+        bert_outs = pad_sequence(bert_outs, batch_first=True) 
         # print("embed after pad bert outs ",bert_outs)
         # print("embed after pad bert outs",bert_outs.size())  
-        # [55, 12, 768]  [55, 12, 768] 表示 BERT 模型已处理了一个批次的 12 个序列，每个序列长度为 55(每个长度又是一个句子)，每个标记的输出是一个大小为 768 的向量。
+        
 
         return bert_outs
 
